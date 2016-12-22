1086d3cf98103f8fec5bf46d5e9bed442351361b
vincent119 <vincent119@gmail.com>
2016-12-22 10:25:51 +0800

    change script

--- elasticsearch-script/elasticsearch_run.sh
+++ elasticsearch-script/elasticsearch_run.sh
@@ -7,7 +7,7 @@
 #
 #
 #
-#   
+#
 ###########################################
 
 export JAVA_HOME=/opt/APP/jdk1.8.0_102
@@ -18,27 +18,34 @@ ES_HOME=/opt/APP/elasticsearch
 CONF_DIR=$ES_HOME/config
 DATA_DIR=$ES_HOME/data
 LOG_DIR=/opt/logs/elasticsearch
-
+pg_name=elasticsearch
 
 prgStart(){
-
-daemon $ES_HOME/bin/elasticsearch -d -Des.pidfile=${ES_HOME}/elasticsearch.pid \
+  echo -n "Starting $pg_name : "
+  daemon $ES_HOME/bin/elasticsearch -d -Des.pidfile=${ES_HOME}/elasticsearch.pid \
                            -Des.default.path.home=${ES_HOME} \
                            -Des.default.path.logs=${LOG_DIR} \
                            -Des.default.path.data=${DATA_DIR} \
                            -Des.default.path.conf=${CONF_DIR}
+  RETVAL=$?
+  echo
+  [ $RETVAL -eq 0 ]
+  return $retval
 }
 
 prgStop(){
+  echo -n "Stoping $pg_name : "
   killproc -p ${ES_HOME}/elasticsearch.pid
-
+  RETVAL=$?
+  echo
+  [ $RETVAL -eq 0 ]
+  return $retval
 }
-
 
 case "$1" in
  start)
    prgStart
-   exit 0;
+   #RETVAL=$?
  ;;
  stop)
    prgStop
@@ -53,5 +60,4 @@ case "$1" in
    status -p ${ES_HOME}/elasticsearch.pid
  ;;
  *)
- echo "usage: $0 {start|stop|restart|status}"
-esac
+ echo
